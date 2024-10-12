5f348c1155a1c3afa9b74c7ceec6dc12abebffc7
Milan Singh <milan083821@gmail.com>
2024-10-12 08:49:13 +0530

    deploying

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/https://github.com/milan-sh/weather-app.git",
+  base: "/weather-app",
   plugins: [react()],
 })
